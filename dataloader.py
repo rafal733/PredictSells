42df9b05e03f1a611ae65267dc4850bc297a731e
rafal733 <rafal733@poczta.onet.pl>
2025-05-23 14:13:38 +0200

    start

---
+++ dataloader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+class DataLoader:
+    def __init__(self):
+        self.sellsData = pd.DataFrame
+        self.bomData = pd.DataFrame
+
+    def load_sells_data_from_file(self, path):
+        pass
+
+    def load_bom_data_from_file(self, path):
+        pass
